56d341df8e05071d47e14144a6b470b85b2f9957
Paramvir Ramola <ramolaparamvir@gmail.com>
2020-09-30 12:51:44 +0530

    added variables for storing dom values, removed eval function and Ui update for output

--- app.js
+++ app.js
@@ -1,95 +1,105 @@
-function getHisory() {
-    return document.getElementById("history-value").innerText;
+var hist = "";
+var output = "";
+
+function getHist() {
+  return (hist = document.getElementById("history-value").innerText);
 }
 
-function printHistory(val) {
-    document.getElementById("history-value").innerText = val;
+function printHist() {
+  document.getElementById("history-value").innerText = hist;
 }
 
-function getOutput(){
-    return document.getElementById("output-value").innerText;
+function getOutput() {
+  return (output = document.getElementById("output-value").innerText);
 }
 
-function printOutput(val){
-    if(val==""){
-        document.getElementById("output-value").innerText = val;
-    }
-    else {
-        document.getElementById("output-value").innerText = getFormatted(val);
-    }
-    
+function printOutput() {
+  if (output == "") {
+    document.getElementById("output-value").innerText = output;
+  } else {
+    document.getElementById("output-value").innerText = getFormatted(output);
+  }
 }
 
-function getFormatted(val){
-    if(val == "-"){
-        return "";
-    }
-    var n = Number(val);
-    return Number(val).toLocaleString("en");
+function getFormatted(val) {
+  if (val == "-") {
+    return "";
+  }
+  var n = Number(val);
+  if(n > 999999999){
+      return n.toExponential(4).toString();
+  }
+  return Number(val).toLocaleString("en");
 }
 
+function evaluate() {
+  console.log("evaluating", hist);
+  return new Function("return " + hist)();
+}
 
-function reverseNumberFormat(val){
-    return Number(val.replace(/,/g,''));
+function reverseNumberFormat(val) {
+  return Number(val.replace(/,/g, ""));
 }
 var operators = document.getElementsByClassName("operator");
-for(var i=0;i<operators.length;i++){
-    operators[i].addEventListener('click', function() {
-        if(this.id=="clear"){
-            printHistory("");
-            printOutput("");
-        } else if(this.id == "backspace"){
+for (var i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", function () {
+    if (this.id == "clear") {
+      output = "";
+      hist = "";
+      printHist();
+      printOutput();
+    } else if (this.id == "backspace") {
+      output = reverseNumberFormat(getOutput()).toString();
 
-            var output = reverseNumberFormat(getOutput()).toString();
+      if (output) {
+        output = output.substr(0, output.length - 1);
+        printOutput();
+      }
+    } else {
+      output = getOutput();
+      hist = getHist();
 
-            if(output){
-                output = output.substr(0,output.length-1)
-                printOutput(output);
-            }
+      console.log(hist + " ----  " + output);
+
+      if (output == "" && hist == "" && this.id == "-") {
+        printHist("0-");
+      }
+
+      if (output == "" && hist != "") {
+        // change operator
+        if (isNaN(hist[hist.length - 1])) {
+          hist = hist.substr(0, hist.length - 1);
         }
-        else{
+      }
 
-            var output = getOutput();
-            var history = getHisory();
-
-            if(output == "" && history == "" && this.id == "-"){
-                printHistory("0-");
-            }
-
-            if(output == "" && history != ""){
-                // change operator
-                if(isNaN(history[history.length - 1])){
-                    history = history.substr(0,history.length -1);
-                }
-            }
-
-
-
-            if(output != "" || history != ""){
-                output = output == "" ? output : reverseNumberFormat(output);
-                history = history + output;
-                if(this.id == "="){
-                    printOutput(eval(history));
-                    printHistory("");
-                }
-                else {
-                    history += this.id;
-                    printHistory(history);
-                    printOutput("");
-                }
-
-            }
+      if (output != "" || hist != "") {
+        output = output == "" ? output : reverseNumberFormat(output);
+        hist = hist + output;
+        if (this.id == "=") {
+          output = evaluate();
+          printOutput();
+          hist = "";
+          printHist();
+        } else {
+          hist += this.id;
+          console.log(this.id, hist);
+          printHist();
+          output = "";
+          printOutput();
         }
-    })
+      }
+    }
+  });
 }
 
 var nums = document.getElementsByClassName("number");
-for (var i=0;i<nums.length;i++){
-    nums[i].addEventListener('click', function() {
-        var output = reverseNumberFormat(getOutput());
-        if(output != NaN){
-            output += this.id;
-            printOutput(output);
-        }
-    })
+for (var i = 0; i < nums.length; i++) {
+  nums[i].addEventListener("click", function () {
+    var len = output.length;
+    output = reverseNumberFormat(getOutput());
+    if (output != NaN && len < 11) {
+      output += this.id;
+      printOutput();
+    }
+  });
 }
